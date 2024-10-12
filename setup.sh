c4f89b051ea0bdc963ea13e0898a4e56e0b642f9
Robert Petersen <robert@orangefood.com>
2024-10-12 16:15:37 -0700

    handle non-zero curl exit for HEAD call

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 handle_error() {
-    echo "An error occurred on line $1"
-    exit 1
+    local exit_code=$?  # Captures the exit status of the last command
+    local error_line=$1  # The line number where the error occurred
+    local command="$BASH_COMMAND"  # The command that was being executed
+
+    echo "Error occurred on line $error_line."
+    echo "Command: $command"
+    echo "Exit code: $exit_code"
+    exit $exit_code  # Exit with the same error code that caused the trap
 }
+
 
 trap 'handle_error $LINENO' ERR
 
@@ -35,7 +42,10 @@ while true; do
   else
     echo "Remote step : ${STEP_FILE}"
     # grab the step from the repo if it exists
-    HTTP_CODE=$(curl -s -w "%{http_code}" -X HEAD https://raw.githubusercontent.com/roost-cc/tools/refs/heads/main/setup/${STEP_FILE})
+    echo curl -s -w \"%{http_code}\" -X HEAD https://raw.githubusercontent.com/roost-cc/tools/refs/heads/main/setup/${STEP_FILE}
+    set +e
+    HTTP_CODE=$(curl -s -o /dev/null -w "%{http_code}" -X HEAD "https://raw.githubusercontent.com/roost-cc/tools/refs/heads/main/setup/${STEP_FILE}" || true)
+    set -e
     if [ $HTTP_CODE -ne 200 ]; then
       echo No step $STEP
       exit
