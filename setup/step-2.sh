4d337084e1e59789b2cef5bce7feaa094635b6d3
Robert Petersen <robert@orangefood.com>
2024-10-12 14:34:10 -0700

    added the chrome permission fix

--- setup/step-2.sh
+++ setup/step-2.sh
@@ -9,5 +9,8 @@ trap 'handle_error $LINENO' ERR
 #       ln this setup to bin directory as setup-roost or something
 ROOST_DIR=$(readlink -f .)
 echo "Step 2 : Setting up 'nix-shell' : ${ROOST_DIR}..."
+# setsup the environment, and pulls the tools
 nix-shell --command "git clone https://github.com/roost-cc/tools.git"
+# fix chrome
+sudo chmod 4755 $(find /nix -name chrome-sandbox)
 echo "Step 2 complete"
